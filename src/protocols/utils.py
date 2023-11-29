e4ca82694686ccac96b1056b8c7282c81fc11b8c
Diogo Matos <diogomatosemail@gmail.com>
2023-11-29 23:23:45 +0000

    fix

--- src/protocols/utils.py
+++ src/protocols/utils.py
@@ -8,7 +8,7 @@ TCP_PORT = 9090
 # set udp server port
 UDP_PORT = 9090
 # set timeout time
-TIMEOUT_TIME = 1
+TIMEOUT_TIME = 5
 
 def chunkify(data: bytes) -> set[tuple[int, bytes]]:
         data_array = bytearray(data)
